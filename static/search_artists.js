1f48db5aa80a7614343b01d7e916155eb18332ed
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2025-01-23 08:53:34 +0100

    Update search_artists.js

--- static/search_artists.js
+++ static/search_artists.js
@@ -48,8 +48,8 @@ async function searchArtist() {
       <p class="select">select</p><input type="checkbox" class="checker">
     </div>
                 
-          <img src="${artist.images[0].url}" id="imge">
-      <div class="div2">            
+      <div class="div2">          
+                <img src="${artist.images[0].url}" id="imge">
               <p class="artistname">${artist.name}</p>
                  </div>
               <p class="figtxt">${formatFollowers(artist.followers.total)} Monthly Listeners</p>
